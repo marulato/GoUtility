40fc7566fad3f45b3c4f7af537e64d30ddd91859
shiba inu <351975711@qq.com>
2019-05-22 22:33:03 +0800

    add functions

--- src/arraylist/arraylist.go
+++ src/arraylist/arraylist.go
@@ -1,13 +1,13 @@
-package arraylist
+package arrayList
 
 type ArrayList struct {
-	Size int
+	size     int
 	capacity int
-	content []interface{}
+	content  []interface{}
 }
 
 func (array *ArrayList) init() *ArrayList {
-	array.Size = 0
+	array.size = 0
 	array.content = make([]interface{}, 0)
 	return array
 }
@@ -16,35 +16,38 @@ func New() *ArrayList {
 	return new(ArrayList).init()
 }
 
+func (array *ArrayList) Size() int {
+	return array.size
+}
+
 func (array *ArrayList) Add(obj interface{}) {
 	array.content = append(array.content, obj)
-	array.Size ++
+	array.size++
 }
 
 func (array *ArrayList) Get(index int) interface{} {
-	if index >= 0 && index < array.Size {
+	if index >= 0 && index < array.size {
 		return array.content[index]
 	}
 	return nil
 }
 
-func (array *ArrayList) IsEmpty() bool  {
-	return array.Size == 0
+func (array *ArrayList) IsEmpty() bool {
+	return array.size == 0
 }
 
-
 func (array *ArrayList) Remove(index int) {
-	if index >= 0 && index < array.Size {
-		frontSlice := array.content[0 : index]
-		backSlice := array.content[index + 1 : array.Size]
+	if index >= 0 && index < array.size {
+		frontSlice := array.content[0:index]
+		backSlice := array.content[index+1 : array.size]
 		array.content = frontSlice
 		array.content = append(array.content, backSlice...)
-		array.Size --
+		array.size--
 	}
 }
 
 func (array *ArrayList) LastIndexOf(obj interface{}) int {
-	if array.Size > 0 {
+	if array.size > 0 {
 		lastIndex := -1
 		for idx, ctn := range array.content {
 			if obj == ctn {
@@ -60,6 +63,9 @@ func (array *ArrayList) Contains(obj interface{}) bool {
 	return array.LastIndexOf(obj) >= 0
 }
 
-func (array *ArrayList) AddAll (objList []interface{}) {
-
+func (array *ArrayList) AddAll(objList *ArrayList) {
+	if objList != nil && objList.Size() > 0 {
+		array.content = append(array.content, objList.content...)
+		array.size += objList.Size()
+	}
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	arrayList "arraylist"
+	"fmt"
+)
+
 func main() {
-	
+	mylist := arrayList.New()
+	mylist.Add("hello")
+	mylist.Add("Golang")
+
+	secList := arrayList.New()
+	secList.Add("hello")
+	secList.Add("java")
+
+	mylist.AddAll(secList)
+	fmt.Println(mylist.Size())
+	for i := 0; i < mylist.Size(); i++ {
+		fmt.Println(mylist.Get(i))
+	}
 }
